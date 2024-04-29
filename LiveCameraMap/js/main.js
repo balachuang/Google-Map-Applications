afca3ccf9d16b20f45d6cbaa27290166491674ad
balachuang <balachuang@gmail.com>
2024-04-29 21:54:20 +0800

    change camera icon

--- LiveCameraMap/js/main.js
+++ LiveCameraMap/js/main.js
@@ -55,7 +55,7 @@ function initGoogle()
             title: camePos[i].title,
             zIndex: 100,
             icon: {
-                path: 'M0 0 L15 -5 L15 5 Z M-2 0 A1 1 0 0 0 2 0 A1 1 0 0 0 -2 0 Z',
+                path: 'M 0 0 L 15 -5 L 15 5 Z M 2 0 A 1 1 0 0 0 -2 0 A 1 1 0 0 0 2 0',
                 fillColor: 'yellow',
                 fillOpacity: 0.6,
                 strokeColor: 'blue',
