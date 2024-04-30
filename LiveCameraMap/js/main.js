d0e99473640313839f4b5428224e74cbeb3d456a
balachuang <balachuang@gmail.com>
2024-04-30 20:03:08 +0800

    bug: cannot display camera point - try 1

--- LiveCameraMap/js/main.js
+++ LiveCameraMap/js/main.js
@@ -56,7 +56,7 @@ function initGoogle()
             title: cameTitle, position: camePosition, 
             map: mapView, draggable: false, zIndex: 200,
             icon: {
-                path: 'M 4 0 A 1 1 0 0 0 -4 0 A 1 1 0 0 0 4 0 Z',
+                path: 'M 4 0 A 1 1 0 0 0 -4 0 A 1 1 0 0 0 4 0',
                 fillColor: 'blue',
                 fillOpacity: 1,
                 strokeColor: 'blue',
@@ -72,10 +72,10 @@ function initGoogle()
             map: mapView, draggable: false, zIndex: 100,
             icon: {
                 path: 'M 0 0 L 20 -8 C 21 -3 21 3 20 8 Z',
-                fillColor: 'yellow',
-                fillOpacity: 0.6,
+                fillColor: 'blue',
+                fillOpacity: 0,
                 strokeColor: 'blue',
-                strokeWeight: 3,
+                strokeWeight: 0,
                 strokeOpacity: 0.6,
                 rotation: camePos[i].angle,
                 scale: 2,
