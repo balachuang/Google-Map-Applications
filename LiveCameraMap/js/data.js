5d633f9ac1ddc1134321029206f896ce64ade051
balachuang <balachuang@gmail.com>
2024-04-30 21:28:53 +0800

    arrange cameras

--- LiveCameraMap/js/data.js
+++ LiveCameraMap/js/data.js
@@ -1,6 +1,14 @@
 var camePos = [
+    // 新店
     {'title': '新北市碧潭',     'position': {'lng': 24.95592248953202, 'lat': 121.53516184925392}, 'angle': -20, 'url': 'https://www.youtube.com/watch?v=6wDmAmr7Lqg' },
-    {'title': '擎天崗草原',     'position': {'lng': 25.16468848295197, 'lat': 121.57558367401835}, 'angle': 100, 'url': 'https://www.youtube.com/watch?v=MnODaP-1JaE' },
     {'title': '北新路寶橋路口', 'position': {'lng': 24.97290306043396, 'lat': 121.54293743810781}, 'angle': -90, 'url': 'https://taiwancctv.live/taiwan/newtaipei/231xindian/get_id.html?id=C000150' },
+
+    // 台北
+    {'title': '擎天崗草原',     'position': {'lng': 25.16468848295197, 'lat': 121.57558367401835}, 'angle': 100, 'url': 'https://www.youtube.com/watch?v=MnODaP-1JaE' },
+
+    // 日本
+    {'title': '函館山百萬夜景', 'position': {'lng': 41.759218473253824, 'lat': 140.70409562721238}, 'angle': -45, 'url': 'https://www.youtube.com/watch?v=s--MDmshT3I&list=PLtPJtE4_gezc20D5xv6MYtjPIti8xLWRZ' },
+
+    // Ending
     {'title':  '', 'position': {'lng': 0.0, 'lat': 0.0}, 'angle': 0, 'url': 'url' }
 ];
