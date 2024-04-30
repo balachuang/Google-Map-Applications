045cb64d5876917b61eee2dbf9166038dab2bd41
balachuang <balachuang@gmail.com>
2024-04-30 23:29:33 +0800

    set zoom only again

--- LiveCameraMap/js/main.js
+++ LiveCameraMap/js/main.js
@@ -63,7 +63,7 @@ function onClickCamera(idx)
     mapView.setZoom(12);
 
     alert('set center: ' + cameraInfo[idx].position.lng + ',' + cameraInfo[idx].position.lat);
-    mapView.setCenter(new google.maps.LatLng({lng: cameraInfo[idx].position.lng, lat: cameraInfo[idx].position.lat}));
+    //mapView.setCenter(new google.maps.LatLng({lng: cameraInfo[idx].position.lng, lat: cameraInfo[idx].position.lat}));
 
     // test
     //window.open(cameraInfo[idx].url, '_blank');
