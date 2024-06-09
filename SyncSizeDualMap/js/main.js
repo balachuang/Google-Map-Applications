571d13cdc064448e61c3fa76094a1e5ffe64c0b6
balachuang <balachuang@gmail.com>
2024-06-09 20:49:15 +0800

    set different event handler for diff google map

--- SyncSizeDualMap/js/main.js
+++ SyncSizeDualMap/js/main.js
@@ -1,7 +1,8 @@
 let glViewLt = null;
 let glViewRt = null;
-// let coder = null;
+let currentActiveMap = 0;
 
+$(window).resize(onReSize);
 $(document).ready(function(){
 	$('#menubar').load('../menubar.html', function(){
 	    $('#nav-LF').addClass('active');
@@ -17,31 +18,38 @@ $(document).ready(function(){
 	});
 });
 
-$(window).resize(onReSize);
-
 function onReSize()
 {
     var sh = $(window).height() - $('#idx-navbar').height() - 20;
-    $('#google-map-container-lt').height(sh);
-    $('#google-map-container-rt').height(sh);
+    $('#gMapL').height(sh);
+    $('#gMapR').height(sh);
 }
 
 function initGoogle()
 {
-    // coder = new google.maps.Geocoder();
-
     let pos = new google.maps.LatLng({ lat: 24.978606, lng: 121.540033 });
     glViewLt = new google.maps.Map(document.getElementById('google-map-container-lt'), { center: pos, zoom: 16 });
     glViewRt = new google.maps.Map(document.getElementById('google-map-container-rt'), { center: pos, zoom: 16 });
 
-    // set zoom handler
+    // set google map container handler
+    $('#gMapL').on('mouseenter', activeLeftGMap);
+    $('#gMapR').on('mouseenter', activeRightGMap);
+}
+
+function activeLeftGMap()
+{
+    google.maps.event.clearListeners(glViewRt, 'zoom_changed');
     glViewLt.addListener('zoom_changed', function(){ afterChangeZoomLevel(true); });
+}
+
+function activeRightGMap()
+{
+    google.maps.event.clearListeners(glViewLt, 'zoom_changed');
     glViewRt.addListener('zoom_changed', function(){ afterChangeZoomLevel(false); });
 }
 
 function afterChangeZoomLevel(isLeft)
 {
-    // stop zooming
     if (isLeft) glViewRt.setZoom(glViewLt.getZoom());
     else        glViewLt.setZoom(glViewRt.getZoom());
 }
