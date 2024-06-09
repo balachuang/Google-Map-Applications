f1a3fef2cad6249be69ab14b1fb24391ad7f3377
balachuang <balachuang@gmail.com>
2024-06-09 21:45:22 +0800

    add loading=async in javascript reference

--- SyncSizeDualMap/js/main.js
+++ SyncSizeDualMap/js/main.js
@@ -31,7 +31,7 @@ function onReSize()
     $('#gMapR').height(sh);
 }
 
-async function initGoogle()
+function initGoogle()
 {
     let pos = new google.maps.LatLng({ lat: 24.978606, lng: 121.540033 });
     glViewLt = new google.maps.Map(document.getElementById('gMapL'), { center: pos, zoom: 16 });
