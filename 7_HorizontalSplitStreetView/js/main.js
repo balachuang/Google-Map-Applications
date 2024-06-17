2458a92fd99590d3837092f17b2a1cc887199e87
balachuang <balachuang@gmail.com>
2024-06-17 20:43:50 +0800

    remove useless code

--- 7_HorizontalSplitStreetView/js/main.js
+++ 7_HorizontalSplitStreetView/js/main.js
@@ -105,124 +105,18 @@ function streetPosChanged()
 
     var heading = glStreetView.getPov().heading;
     console.log('angle: ' + heading);
-    // glMarkerAr.rotaion = Math.PI * heading / 180;
-    // glMarkerAr.icon.rotaion = heading;
-    // glMarkerAr.setIcon({
-    //     path: 'M -3 0 L -4 -4 L 6 0 L -4 4 Z',
-    //     fillColor: 'blue',
-    //     fillOpacity: 1,
-    //     strokeWeight: 0,
-    //     rotation: heading,
-    //     scale: 2,
-    //     anchor: new google.maps.Point(0, 0)
-    // });
     glMarkerAr.setIcon({
-        rotation: heading
+        path: 'M -0 3 L -4 4 L 0 -6 L 4 4 Z',
+        fillColor: 'blue',
+        fillOpacity: 1,
+        strokeWeight: 0,
+        rotation: heading,
+        scale: 2,
+        anchor: new google.maps.Point(0, 0)
     });
 }
 
-// function panoPovChange(idx)
-// {
-//     if (idx == 0) return;
-//     setDirectionLine(idx);
-
-//     var tpos = calTargetPoint();
-//     if (tpos != null) {
-//         glTarget.setPosition(tpos);
-//         coder.geocode({ location: tpos }, function(result, status){
-//             if (status === google.maps.GeocoderStatus.OK)
-//                 glTarget.setOptions({ title: result[0].formatted_address });
-//         });
-//     }
-// }
-
-// function setDirectionLine(idx)
-// {
-//     if (idx == 0) return;
-
-//     var heading = glView[idx].getPov().heading;
-//     var x = 0.1 * Math.sin(Math.PI * heading / 180);
-//     var y = 0.1 * Math.cos(Math.PI * heading / 180);
-
-//     glPath[idx].setPath([glPos[idx], {lat: glPos[idx].lat() + y, lng: glPos[idx].lng() + x}]);
-// }
-
 function resetPosByCenter(lat, lng) { glCurrPos = new google.maps.LatLng({ lat: lat, lng: lng }); }
-
-// function calTargetPoint()
-// {
-//     var x1 = [0, 0, 0];
-//     var x2 = [0, 0, 0];
-//     var y1 = [0, 0, 0];
-//     var y2 = [0, 0, 0];
-//     var vecX = [0, 0, 0];
-//     var vecY = [0, 0, 0];
-//     var length = [0, 0, 0];
-
-//     for (var n=1; n<=2; ++n)
-//     {
-//         var heading = glView[n].getPov().heading;
-//         var x = 0.1 * Math.sin(Math.PI * heading / 180);
-//         var y = 0.1 * Math.cos(Math.PI * heading / 180);
-
-//         x1[n] = glPos[n].lng();
-//         y1[n] = glPos[n].lat();
-//         x2[n] = glPos[n].lng() + x;
-//         y2[n] = glPos[n].lat() + y;
-//     }
-
-//     for (var n=1; n<=2; ++n)
-//     {
-//         vecX[n] = x2[n] - x1[n];
-//         vecY[n] = y2[n] - y1[n];
-//         length[n] = Math.sqrt(vecX[n] * vecX[n] + vecY[n] * vecY[n]);
-//         vecX[n] = vecX[n] / length[n];
-//         vecY[n] = vecY[n] / length[n];
-//     }
-
-//     var denominator = vecX[1] * vecY[2] - vecY[1] * vecX[2];
-//     var molecular1 = x1[2] - x1[1];
-//     var molecular2 = y1[2] - y1[1];
-//     var ta = (vecY[2] * molecular1 - vecX[2] * molecular2) / denominator;
-//     var tb = (vecY[1] * molecular1 - vecX[1] * molecular2) / denominator;
-//     if ((0 > ta) || (ta > 1) || (0 > tb) || (tb > 1)) {
-//         glTarget.setOpacity(0);
-//         return null;
-//     }
-
-//     glTarget.setOpacity(1 - 1 / (1 + Math.exp(6.5 - 100 * Math.max(ta, tb))));
-
-//     var tx1 = x1[1] + ta * vecX[1];
-//     var tx2 = x1[2] + tb * vecX[2];
-//     var ty1 = y1[1] + ta * vecY[1];
-//     var ty2 = y1[2] + tb * vecY[2];
-
-//     var targetPos = new google.maps.LatLng({ lat: (ty1+ty2)/2, lng: (tx1+tx2)/2 });
-//     return targetPos;
-// }
-
-// function onClickTarget()
-// {
-//     glTargetView = glView[0].getStreetView();
-//     glTargetView.setPosition(glTarget.getPosition());
-//     glTargetView.setVisible(true);
-// }
-
-// function checkStatus()
-// {
-//     for (var n=1; n<=2; ++n) {
-//         if (glView[n].getStatus() === google.maps.StreetViewStatus.OK)
-//         {
-//             glMarker[n].setOpacity(1);
-//             glPath[n].setOptions({strokeOpacity: 0.3});
-//             glView[n].setVisible(true);
-//         }else{
-//             glMarker[n].setOpacity(0.3);
-//             glPath[n].setOptions({strokeOpacity: 0});
-//             glView[n].setVisible(false);
-//         }
-//     }
-// }
 
 // add Range by SVG
 function addCameraRange(idx)
