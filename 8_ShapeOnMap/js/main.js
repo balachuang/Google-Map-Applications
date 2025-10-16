f446b21ad9b9a31e7da756730fd9e045cace526e
balachuang <balachuang@gmail.com>
2025-10-16 21:18:00 +0800

    add manually input function to Shape on Map

--- 8_ShapeOnMap/js/main.js
+++ 8_ShapeOnMap/js/main.js
@@ -5,6 +5,9 @@ var mapView = null;
 var mapCircle;
 var mapRectangle;
 
+var circleEventHandlerId;
+var rectangleEventHandlerId;
+
 var initPoint = {
 	lat: 24.978899291207604,
 	lng: 121.54248131780544
@@ -12,13 +15,13 @@ var initPoint = {
 
 var shapeDisplay = {
 	stroke: {
-		color: '#FF0000',
-		opacity: 0.8,
-		weight: 2,
+		color: '#0000FF',
+		opacity: 0.5,
+		weight: 4,
 	},
 	fill: {
-		color: '#FF0000',
-		opacity: 0.35,
+		color: '#0000FF',
+		opacity: 0.15,
 	},
 };
 
@@ -74,24 +77,105 @@ function initGoogle()
 	}
 
 	// 自訂控制項, 其實就是自己做一個 dom object, 然後用 google api 放進 map 裡.
-	mapView.controls[google.maps.ControlPosition.TOP_RIGHT].push(document.getElementById("shape-selector-container"));
+	mapView.controls[google.maps.ControlPosition.TOP_CENTER].push(document.getElementById("shape-selector-container"));
 	$('#shape-selector-container').show();
-	$('#shape-selector').change(changeShape);
+	$('#shape-selector').change(switchChape);
+	$('#circle-info-radius').change(updateCircleRadius);
+	$('#rectangle-info-width').change(updateRectangleDimension);
+	$('#rectangle-info-height').change(updateRectangleDimension);
 
 	// for init only.
 	initShape();
 	mapCircle.setMap(mapView);
 	$('#circle-info').show();
-
-	// changeShape();
 }
 
-function changeShape(e)
+function initShape()
 {
-	let shapeName = $('#shape-selector').val();
-	console.log(shapeName);
+	mapCircle = new google.maps.Circle({
+		strokeColor: shapeDisplay.stroke.color,
+		strokeOpacity: shapeDisplay.stroke.opacity,
+		strokeWeight: shapeDisplay.stroke.weight,
+		fillColor: shapeDisplay.fill.color,
+		fillOpacity: shapeDisplay.fill.opacity,
+		draggable: true,
+		editable: true,
+		center: initPoint,
+		radius: 5000,
+	});
+	circleEventHandlerId = mapCircle.addListener("radius_changed", showCircleInfo);
 
-	switch(shapeName)
+	mapRectangle = new google.maps.Rectangle({
+		strokeColor: shapeDisplay.stroke.color,
+		strokeOpacity: shapeDisplay.stroke.opacity,
+		strokeWeight: shapeDisplay.stroke.weight,
+		fillColor: shapeDisplay.fill.color,
+		fillOpacity: shapeDisplay.fill.opacity,
+		draggable: true,
+		editable: true,
+		// manually calculate
+		bounds: {
+			north: 25.024,
+			south: 24.934,
+			east: 121.592,
+			west: 121.493,
+		},
+	});
+	rectangleEventHandlerId = mapRectangle.addListener("bounds_changed", showRectangleInfo);
+}
+
+// read new radius from input and update to Circle object
+function updateCircleRadius()
+{
+	// remove radius_changed handler to avoid infinit recursive
+	google.maps.event.removeListener(circleEventHandlerId);
+
+	$('#circle-info-radius').blur();
+	let rStr = $('#circle-info-radius').val();
+	try{
+		let r = eval(rStr);
+		mapCircle.setRadius(r);
+	}catch(ex){
+		console.log('Update Raduis Fail' + ex);
+		showCircleInfo();
+	}
+
+	// re-add radius_changed event handler
+	circleEventHandlerId = mapCircle.addListener("radius_changed", showCircleInfo);
+}
+
+// read new radius from input and update to Circle object
+function updateRectangleDimension()
+{
+	// remove bounds_changed handler to avoid infinit recursive
+	google.maps.event.removeListener(rectangleEventHandlerId);
+
+	$('#rectangle-info-width').blur();
+	$('#rectangle-info-height').blur();
+	let wStr = $('#rectangle-info-width').val();
+	let hStr = $('#rectangle-info-height').val();
+	try{
+		let w = eval(wStr);
+		let h = eval(hStr);
+
+		const top  = mapRectangle.getBounds().getNorthEast().lat();
+		const left = mapRectangle.getBounds().getSouthWest().lng();
+		const newNe = google.maps.geometry.spherical.computeOffset({lat: top, lng: left}, w, 90);
+		const newSw = google.maps.geometry.spherical.computeOffset({lat: top, lng: left}, h, 180);
+
+		mapRectangle.setBounds(new google.maps.LatLngBounds(newSw, newNe));
+	}catch(ex){
+		console.log('Update Raduis Fail' + ex);
+		showRectangleInfo();
+	}
+
+	// re-add bounds_changed event handler
+	rectangleEventHandlerId = mapRectangle.addListener("bounds_changed", showRectangleInfo);
+}
+
+function switchChape(e)
+{
+	switch($('#shape-selector').val())
 	{
 		case 'circle':
 			mapCircle.setMap(mapView);
@@ -112,43 +196,9 @@ function changeShape(e)
 	}
 }
 
-function initShape()
-{
-	mapCircle = new google.maps.Circle({
-		strokeColor: shapeDisplay.stroke.color,
-		strokeOpacity: shapeDisplay.stroke.opacity,
-		strokeWeight: shapeDisplay.stroke.weight,
-		fillColor: shapeDisplay.fill.color,
-		fillOpacity: shapeDisplay.fill.opacity,
-		draggable: true,
-		editable: true,
-		center: initPoint,
-		radius: 5000,
-	});
-	mapCircle.addListener("radius_changed", showCircleInfo);
-
-	mapRectangle = new google.maps.Rectangle({
-		strokeColor: shapeDisplay.stroke.color,
-		strokeOpacity: shapeDisplay.stroke.opacity,
-		strokeWeight: shapeDisplay.stroke.weight,
-		fillColor: shapeDisplay.fill.color,
-		fillOpacity: shapeDisplay.fill.opacity,
-		draggable: true,
-		editable: true,
-		// manually calculate
-		bounds: {
-			north: 25.024,
-			south: 24.934,
-			east: 121.592,
-			west: 121.493,
-		},
-	});
-	mapRectangle.addListener("bounds_changed", showRectangleInfo);
-}
-
 function showCircleInfo() {
 	const r = Math.round(mapCircle.getRadius() * 100) / 100;
-	$('#circle-info-radius').text(r);
+	$('#circle-info-radius').val(r);
 }
 
 function showRectangleInfo()
@@ -169,8 +219,8 @@ function showRectangleInfo()
 	);
 	width  = Math.round(width * 100) / 100;
 	height = Math.round(height * 100) / 100;
-	$('#rectangle-info-width' ).text(width);
-	$('#rectangle-info-height').text(height);
+	$('#rectangle-info-width' ).val(width);
+	$('#rectangle-info-height').val(height);
 }
 
 // 手動計算已知中心點, 邊長為 10000 m 的方形 
